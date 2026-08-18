3ebbbcca16d95dc12275210fbd786a581b75eed7
Mark Robinson <mark.robinson@mls.uzh.ch>
2026-08-18 11:20:15 +0200

    reroute imports

--- run.py
+++ run.py
@@ -15,7 +15,6 @@ import scanpy as sc
 repo_dir = Path(__file__).parent
 sys.path.insert(0, str(repo_dir))
 
-from methods import run_scanpy, run_rapids
 
 
 # Parse command line arguments
@@ -107,6 +106,7 @@ n_cluster = clusters_truth_num + args.d_cluster
 
 
 if args.method_name == "scanpy":
+    from methods import run_scanpy
     adata = run_scanpy(
         adata,
         n_cluster,
@@ -118,6 +118,7 @@ if args.method_name == "scanpy":
         clustering_info,
     )
 elif args.method_name == "rapids":
+    from methods import run_rapids
     adata = run_rapids(
         adata,
         n_cluster,
